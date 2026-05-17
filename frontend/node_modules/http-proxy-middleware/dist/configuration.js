f787a12c238256a668a53fcefe80c2744896ab5b
石敬荣 <15920017+itjingrong@user.noreply.gitee.com>
2026-05-18 02:51:49 +0800

    Remove target and node_modules from git tracking

--- frontend/node_modules/http-proxy-middleware/dist/configuration.js
+++
@@ -1,6 +0,0 @@
-import { ERRORS } from './errors.js';
-export function verifyConfig(options) {
-    if (!options.target && !options.router) {
-        throw new Error(ERRORS.ERR_CONFIG_FACTORY_TARGET_MISSING);
-    }
-}
